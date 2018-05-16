8382bfecc0c25f44b444b2345356f19aa9ff16b1
ArneDJ <ArneDeJonghe@protonmail.com>
2018-05-16 20:20:13 +0200

    version 2.1

--- BiometricStation/BiometricStation.ino
+++ BiometricStation/BiometricStation.ino
@@ -1,3 +1,4 @@
+//Author: Arne De Jonghe
 
 #include <LiquidCrystal.h>
 #include <Wire.h> 
@@ -27,9 +28,10 @@ const unsigned long baudRate = 9600;
 unsigned long previousMillis = 0;
 unsigned long previousMillis2 = 0;
 
+
 void setup() {
   
-  //Start Temperaturesensor
+  //Start Temperaturesensor setup
   Serial.begin(9600); 
   pinMode(ALERT_PIN,INPUT);  
   sensor0.begin();  
@@ -40,24 +42,19 @@ void setup() {
   sensor0.setExtendedMode(0);
   sensor0.setHighTempF(85.0); 
   sensor0.setLowTempF(84.0);  
-  //End Temperaturesensor
+  //End Temperaturesensor setup
 
-
-  //Start Pulsesensor
-  pinMode(LED13,OUTPUT);         
-
-  //End Pulsesensor 
-
-  //Start Accelerometer
+  //Start Accelerometer setup
   lcd.begin(16, 2);
   accel.init();
-  //End Accelerometer
+  //End Accelerometer setup
 
 
 
 }
 
 
+//Button values
 char* buttons[] = {"Left", "Up", "Down", "Right", "Select", "Undefined"};
 
 #define LEFT_BUTTON     0
@@ -68,7 +65,7 @@ char* buttons[] = {"Left", "Up", "Down", "Right", "Select", "Undefined"};
 #define UNDEFINED       5
 
 int buttonindex;
-
+//Button values
 
 
 void loop() {
@@ -87,8 +84,6 @@ void loop() {
   Serial.print("[");
   Serial.print(temperature);
   Serial.print(" | ");
-  
- 
   //End Temperaturesensor
 
 
@@ -98,15 +93,10 @@ void loop() {
    Serial.print(Signal);
    Serial.print(" | ");     
 
+   unsigned long currentMillis = millis();
 
-    unsigned long currentMillis = millis();
+   if(myTimer1(delayTime, currentMillis) == 1){
 
-  // First event
-  if(myTimer1(delayTime, currentMillis) == 1){
-
-   
-
-    // Heart beat leading edge detected.
     if(Signal > Threshold && IgnoreReading == false){
       if(FirstPulseDetected == false){
         FirstPulseTime = millis();
@@ -118,21 +108,16 @@ void loop() {
         FirstPulseTime = SecondPulseTime;
       }
       IgnoreReading = true;
-      //digitalWrite(LED_BUILTIN, HIGH);
+     
     }
-
-    // Heart beat trailing edge detected.
+   
     if(reading < LowerThreshold && IgnoreReading == true){
       IgnoreReading = false;
-      //digitalWrite(LED_BUILTIN, LOW);
+    
     }  
 
-    // Calculate Beats Per Minute.
     BPM = (1.0/PulseInterval) * 60.0 * 1000;
-  }
-
-  
- 
+   }
    //End Pulsesensor
 
    
@@ -140,66 +125,25 @@ void loop() {
   
 
 
-//Start Accelerometer
+   //Start Accelerometer
 
-   if (accel.available())
-  {
+   if (accel.available()){
   
     accel.read();
-    
-    // accel.read() will update two sets of variables. 
-    // * int's x, y, and z will store the signed 12-bit values 
-    //   read out of the accelerometer.
-    // * floats cx, cy, and cz will store the calculated 
-    //   acceleration from those 12-bit values. These variables 
-    //   are in units of g's.
-    // Check the two function declarations below for an example
-    // of how to use these variables.
     printCalculatedAccels();
-    //printAccels(); // Uncomment to print digital readings
-    
-    // The library also supports the portrait/landscape detection
-    //  of the MMA8452Q. Check out this function declaration for
-    //  an example of how to use that.
     printOrientation();
-    
     Serial.println("]"); 
     delay(100); 
- 
-
 }
-//End Accelerometer
-
-    //Start LCD screen
-//    lcd.clear();
-//    lcd.print(temperature);
-//    lcd.print(" ");
-//    lcd.print((char)223);
-//    lcd.print("C ");
-//    lcd.print(" PPM: ");
-//    lcd.println(Signal);
-//    lcd.print("a: ");
-//    lcd.print(accel.x, 3);
-//    lcd.print("\t");
-//    lcd.print(accel.y, 3);
-//    lcd.print("\t");
-//    lcd.print(accel.z, 3);
-//    lcd.print("\t");
-    //End LCD screen
+    //End Accelerometer
 
 
-
-//Start Buttons
-   //int buttonValue = 1023;
+   //Start Buttons
     int buttonValue = analogRead(A0);
     lcd.clear();
-    lcd.print("Press Any Button");
-   
-//    while (buttonValue > 1015) 
-//    {
-//        buttonValue = analogRead(A0);
-//        //Serial.println(buttonValue);
-//    }
+    lcd.print("Bio Station");
+    lcd.setCursor(0, 2);
+    lcd.print("Arne en Jasper");
 
     if (buttonValue > 845 && buttonValue < 865)  // left button
     {
@@ -240,8 +184,6 @@ void loop() {
     {
         buttonindex = RIGHT_BUTTON;
         lcd.clear();
-//        lcd.print("acceleration: ");
-//        lcd.setCursor(0, 2);
         lcd.print("X:");
         lcd.print(accel.cx, 3);
         lcd.print("g ");
@@ -258,16 +200,6 @@ void loop() {
         buttonindex = SEL_BUTTON;
     }
     else buttonindex = UNDEFINED;
-    //lcd.clear();
-    //lcd.print(buttons[buttonindex]);
-    //lcd.print("  pressed");
-    // wait for key to be releasted
-
-//    while (buttonValue < 1000) 
-//    {
-//        buttonValue = analogRead(A0);
-//        //delay(100000);
-//    };  // sit in this loop till key unpressed
     //End Buttons
 
     
@@ -284,10 +216,7 @@ void printAccels()
   Serial.print("\t");
 }
 
-// This function demonstrates how to use the accel.cx, accel.cy,
-//  and accel.cz variables.
-// Before using these variables you must call the accel.read()
-//  function!
+
 void printCalculatedAccels()
 { 
   Serial.print(accel.cx, 3);
@@ -298,15 +227,9 @@ void printCalculatedAccels()
   Serial.print("\t");
 }
 
-// This function demonstrates how to use the accel.readPL()
-// function, which reads the portrait/landscape status of the
-// sensor.
+
 void printOrientation()
 {
-  // accel.readPL() will return a byte containing information
-  // about the orientation of the sensor. It will be either
-  // PORTRAIT_U, PORTRAIT_D, LANDSCAPE_R, LANDSCAPE_L, or
-  // LOCKOUT.
   byte pl = accel.readPL();
   switch (pl)
   {
@@ -329,7 +252,6 @@ void printOrientation()
 }
 
 
-// First event timer
 int myTimer1(long delayTime, long currentMillis){
   if(currentMillis - previousMillis >= delayTime){previousMillis = currentMillis;return 1;}
   else{return 0;}
